b9a8f2946683144bc44555cdaa6e0ef92bb3865d
Giuseppe Travasoni <giuseppe.travasoni@gmail.com>
2016-08-26 10:25:56 +0200

    changing sdks

--- travis/travis_build_script.sh
+++ travis/travis_build_script.sh
@@ -4,10 +4,10 @@
 set -e
 
 echo "Launching build and test on iOS"
-time xcodebuild build -project Invoke.xcodeproj -scheme Invoke -sdk iphonesimulator -destination 'platform=iOS Simulator,name=iPhone 6s,OS=10.0' | xcpretty
+time xcodebuild test -project Invoke.xcodeproj -scheme Invoke -sdk iphonesimulator10.0 -destination 'platform=iOS Simulator,name=iPhone 6s,OS=10.0' | xcpretty
 
 echo "Launching build and test on tvOS"
-time xcodebuild build -project Invoke.xcodeproj -scheme Invoke -sdk iphonesimulator -destination 'platform=tvOS Simulator,name=Apple TV 1080p,OS=10.0' | xcpretty
+time xcodebuild test -project Invoke.xcodeproj -scheme Invoke -sdk appletvsimulator10.0 -destination 'platform=tvOS Simulator,name=Apple TV 1080p,OS=10.0' | xcpretty
 
 echo "Launching build and test on macOS"
-time xcodebuild build -project Invoke.xcodeproj -scheme Invoke -sdk iphonesimulator -destination 'platform=OS X,arch=x86_64' | xcpretty
+time xcodebuild test -project Invoke.xcodeproj -scheme Invoke -sdk macosx10.11 -destination 'platform=OS X,arch=x86_64' | xcpretty
