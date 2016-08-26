a291563fa93cd1d7601e86ea9e342289894f73e6
Giuseppe Travasoni <giuseppe.travasoni@gmail.com>
2016-08-26 12:30:50 +0200

    tests only on mac for now

--- travis/travis_build_script.sh
+++ travis/travis_build_script.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-echo "Launching build and test on iOS"
-xcodebuild test -project Invoke.xcodeproj -scheme Invoke -sdk iphonesimulator10.0 -destination 'platform=iOS Simulator,name=iPhone 6s,OS=10.0' | xcpretty
+#echo "Launching build and test on iOS"
+#xcodebuild test -project Invoke.xcodeproj -scheme Invoke -sdk iphonesimulator10.0 -destination 'platform=iOS Simulator,name=iPhone 6s,OS=10.0' | xcpretty
 
-echo "Launching build and test on tvOS"
-xcodebuild test -project Invoke.xcodeproj -scheme Invoke -sdk appletvsimulator10.0 -destination 'platform=tvOS Simulator,name=Apple TV 1080p,OS=10.0' | xcpretty
+#echo "Launching build and test on tvOS"
+#xcodebuild test -project Invoke.xcodeproj -scheme Invoke -sdk appletvsimulator10.0 -destination 'platform=tvOS Simulator,name=Apple TV 1080p,OS=10.0' | xcpretty
 
 echo "Launching build and test on macOS"
 xcodebuild test -project Invoke.xcodeproj -scheme Invoke -sdk macosx -destination 'platform=OS X,arch=x86_64' | xcpretty
